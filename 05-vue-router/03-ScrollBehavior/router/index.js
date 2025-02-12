9f24ecc51e53eea3e1465d9138ed7e36e8f84861
Svetlana-Gubina <gubinas83@gmail.com>
2025-02-12 12:34:26 +0300

    ScrollBehavior

--- 05-vue-router/03-ScrollBehavior/router/index.js
+++ 05-vue-router/03-ScrollBehavior/router/index.js
@@ -3,6 +3,26 @@ import { createRouter, createWebHistory } from 'vue-router';
 export const router = createRouter({
   history: createWebHistory('/05-vue-router/03-ScrollBehavior'),
 
+  scrollBehavior(to, from, savedPosition) {
+    if (to.hash) {
+      return {
+        el: to.hash,
+        behavior: 'smooth',
+      };
+    }
+    if (to.meta.saveScrollPosition && from.meta.saveScrollPosition) {
+      return false;
+    }
+    if (savedPosition) {
+      return savedPosition;
+    } else {
+      return {
+        left: 0,
+        top: 0,
+      };
+    }
+  },
+
   routes: [
     {
       path: '/',

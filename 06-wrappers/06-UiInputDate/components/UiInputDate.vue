18e14e48ea98f5ef27a7e4843a7e7be96488abfe
Svetlana-Gubina <gubinas83@gmail.com>
2025-02-20 12:02:04 +0300

    UiInputDate

--- 06-wrappers/06-UiInputDate/components/UiInputDate.vue
+++ 06-wrappers/06-UiInputDate/components/UiInputDate.vue
@@ -1,5 +1,9 @@
 <template>
-  <UiInput />
+  <UiInput :type="type" :step="step" v-model="modelValueProxy">
+    <template v-for="slotName in Object.keys($slots)" #[slotName]>
+      <slot :name="slotName"/>
+    </template>
+  </UiInput>
 </template>
 
 <script>
@@ -7,6 +11,34 @@ import UiInput from './UiInput.vue';
 
 export default {
   name: 'UiInputDate',
+
+  props: {
+    type: {
+      type: String,
+      default: "date"
+    },
+    modelValue: {
+      type: Number,
+      default: 0
+    },
+    step: {
+      type: Number,
+      default: 0
+    },
+  },
+
+  computed: {
+    modelValueProxy: {
+      get() {
+        return this.modelValue.toString();
+      },
+      set(value) {
+        this.$emit('update:modelValue', new Date(value))
+      },
+    },
+  },
+
+  emits: ['update:modelValue'],
 
   components: { UiInput },
 };

a0fccbb1386c64d046ccfc174afeeb95a81dbf47
Svetlana-Gubina <gubinas83@gmail.com>
2025-02-25 12:46:59 +0300

    TemplateRenderer

--- 09-rendering/02-TemplateRenderer/components/TemplateRenderer.vue
+++ 09-rendering/02-TemplateRenderer/components/TemplateRenderer.vue
@@ -1,5 +1,6 @@
 <script>
-// import { compile } from 'vue';
+import { defineComponent, h } from 'vue';
+import { compile } from 'vue';
 
 export default {
   name: 'TemplateRenderer',
@@ -20,5 +21,24 @@ export default {
       default: () => [],
     },
   },
+
+  computed: {
+    renderFunction() {
+      return compile(this.template);
+    },
+
+    componentFromTemplate() {
+      return defineComponent({
+        name: 'TemplateRendererInternal',
+        props: ['bindings'],
+        components: this.components,
+        render: this.renderFunction,
+      })
+    }
+  },
+
+  render() {
+    return h(this.componentFromTemplate, {bindings: this.bindings})
+  }
 };
 </script>

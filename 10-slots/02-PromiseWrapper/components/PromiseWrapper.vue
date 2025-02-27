1de4da9f224124378e704b654c5479281a5581c0
Svetlana-Gubina <gubinas83@gmail.com>
2025-02-27 12:11:01 +0300

    PromiseWrapper

--- 10-slots/02-PromiseWrapper/components/PromiseWrapper.vue
+++ 10-slots/02-PromiseWrapper/components/PromiseWrapper.vue
@@ -1,5 +1,13 @@
 <template>
-  <!-- -->
+  <div v-if="promiseState === 'pending'">
+    <slot name="pending"></slot>
+  </div>
+  <div v-if="promiseState === 'rejected'">
+    <slot name="rejected" :error="promise.error"></slot>
+  </div>
+  <div v-if="promiseState === 'fulfilled'">
+    <slot name="fulfilled" :result="promise.result"></slot>
+  </div>
 </template>
 
 <script>
@@ -11,6 +19,16 @@ export default {
       type: Promise,
       required: true,
     },
+  },
+
+  computed: {
+    promiseState() {
+      return this.promise.state;
+    }
+  },
+
+  mounted(){
+    console.log(this.promise)
   },
 };
 </script>
